cb8f1c57714aabc121d2ae674999fdc191f70a5a
K4re1 <163157009+K4re1@users.noreply.github.com>
2024-03-19 18:22:11 +0300

    Update first window
    Обновлено первое окно

--- my_app.py
+++ my_app.py
@@ -1,1 +1,34 @@
 # напиши здесь код основного приложения и первого экрана
+from instr import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.next_click()
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+
+app = QApplication([])
+main_win1 = MainWin()
+main_win1.show()
+app.exec_()
