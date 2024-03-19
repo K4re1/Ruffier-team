dfedb5453991bf22479b245bd0cd26f18caa8efb
K4re1 <163157009+K4re1@users.noreply.github.com>
2024-03-19 17:39:06 +0300

    Add files via upload

--- instr.py
+++ instr.py
@@ -1,1 +1,26 @@
-# напиши здесь код для хранения текстовых инструкций для приложения
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+txt_hello = 'Добро пожаловать в программу по определению состояния вашего здоровья!'
+txt_next = ''
+txt_instruction = '' 
+txt_title = ''
+txt_name = ''
+txt_hintname = ""
+txt_hintage = ""
+txt_test1 = ''
+txt_test2 = ''
+txt_test3 = ''
+txt_sendresults = ''
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = ''
+txt_starttest2 = ''
+txt_starttest3 = ''
+
+txt_age = ''
+txt_finalwin = ''
+txt_index = ''
+txt_workheart = ''
