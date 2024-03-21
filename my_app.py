29ee4035cca2d96597baf7f80ad8a694f0a75126
K4re1 <163157009+K4re1@users.noreply.github.com>
2024-03-21 16:40:51 +0300

    Update first window
    Внес небольшие изменения

--- my_app.py
+++ my_app.py
@@ -1,7 +1,8 @@
 # напиши здесь код основного приложения и первого экрана
 from instr import *
+from second_win import TestWin
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 
 class MainWin(QWidget):
     def __init__(self):
@@ -9,7 +10,7 @@ class MainWin(QWidget):
         self.set_appear()
         self.initUI()
         self.connects()
-        self.next_click()
+        self.show()
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
@@ -17,18 +18,18 @@ class MainWin(QWidget):
     def initUI(self):
         self.hello = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
+        self.btn_next = QPushButton(txt_next)
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.hello, alignment = Qt.AlignLeft)
         self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
-        self.layout.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)
         self.setLayout(self.layout)
     def connects(self):
-        self.button.clicked.connect(self.next_click)
+        self.btn_next.clicked.connect(self.next_click)
     def next_click(self):
-        self.hide()
-
+        self.close()
+        self.tw = TestWin()
+        
 app = QApplication([])
-main_win1 = MainWin()
-main_win1.show()
+mw = MainWin()
 app.exec_()
